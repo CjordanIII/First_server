fd52e056b755ef6dd54268f7e181a6f7772b3518
Clarence Jordan <clarencejordan087@gmail.com>
2023-08-26 17:59:33 -0500

    done

--- index.js
+++ index.js
@@ -11,20 +11,38 @@ app.engine('madeline',(filePath,option,callback)=>{
         const rendered = content
           .toString()
           .replace("#title#", "<title>" + option.title + "</title>")
-          .replace("#message#", "<h1>" + option.message + "</h1>")
-          .replace("#content#","<div>"+option.content+"</div>");
+          .replace("#message#", "<header>" + option.message + "</header>")
+          .replace("#content#", "<div>" + option.content + "</div>")
+          .replace("#messageToUser#", "<div> <a href=http://localhost:3000/98>" + option.messageToUser + "</a></div>");
           console.log(rendered)
           return callback(null,rendered)
     })
 })
 
+
+
 app.set('views','./views')
 
+
+
 app.get("/",(req,res)=>{
-    res.render('template',{title:'This is title',message:"first template engin",content:'node js and express'})
+    res.render('template',{title:'Week 11 day 2 homework',message:"Bear on the wall",content:'99 Bottles of bee on the wall',messageToUser:"Take one down , pass it around "})
 })
 
+app.get("/:number_of_bottles", (req, res) => {
+  const bottles = parseInt(req.params.number_of_bottles); // Convert parameter to an integer
+  const nextBottles = bottles - 1; // Calculate the next number of bottles
+
+  res.send(`
+        ${bottles} Bottles of beer on the wall
+        <a href="http://localhost:3000/${nextBottles}">Take one down, pass it around</a>
+    `);
+});
+
+
 app.set('view engine',"madeline")
+
+
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`)
@@ -37,5 +55,3 @@ app.listen(port,()=>{
 
 
 
-
-// app.listen(port,()=>console.log(`server running on ${port}`))
